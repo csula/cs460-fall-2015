08c0a6e5bb7468c48a94a29821688a19cde274d8
Eric Liao <rcliao01@gmail.com>
2015-09-22 20:34:03 -0700

    add @Override to HelloWorld.java

--- src/main/java/edu/csula/cs460/helloworld/HelloWorld.java
+++ src/main/java/edu/csula/cs460/helloworld/HelloWorld.java
@@ -1,6 +1,7 @@
 package edu.csula.cs460.helloworld;
 
 public class HelloWorld extends Hello {
+    @Override
     public String saySomething(String message) {
         return "Hello, " + message;
     }
