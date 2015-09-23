3569a1e863224febc9da29f29e6e6e96c53fe511
Eric Liao <rcliao01@gmail.com>
2015-09-22 20:28:46 -0700

    fix unit test failure
    Because I am tired of seeing unit test fail message from Travis

--- src/main/java/edu/csula/cs460/helloworld/HelloWorld.java
+++ src/main/java/edu/csula/cs460/helloworld/HelloWorld.java
@@ -2,8 +2,6 @@ package edu.csula.cs460.helloworld;
 
 public class HelloWorld extends Hello {
     public String saySomething(String message) {
-        // TODO: say Hello, {message}
-
-        return null;
+        return "Hello, " message;
     }
 }
