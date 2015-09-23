c93561be0d91615001d28810c71c7fcba54f333d
Eric Liao <rcliao01@gmail.com>
2015-09-22 20:33:42 -0700

    add `Hello, AI` test case

--- src/test/java/edu/csula/cs460/helloworld/HelloWorldTest.java
+++ src/test/java/edu/csula/cs460/helloworld/HelloWorldTest.java
@@ -1,17 +1,33 @@
 package edu.csula.cs460.helloworld;
 
 import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
+import org.junit.Before;
 
 public class HelloWorldTest {
+    private HelloWorld hello;
+
+    @Before
+    public void setUp() {
+        HelloWorld hello = new HelloWorld();
+    }
+
     @Test
     public void sayHelloAI() {
-        HelloWorld hello = new HelloWorld();
-
         assertEquals(
-            "Test case 1 - `Hello, AI`",
-            "Hello, AI",
-            hello.saySomething("AI")
+            "Test case 1 - `Hello, CS 460`",
+            "Hello, CS 460",
+            hello.saySomething("CS 460")
+        );
+    }
+
+    @Test
+    public void sayHelloAI() {
+        assertEquals(
+            "Test case 2 - `Hello, Artificial Intelligence`",
+            "Hello, Artificial Intelligence",
+            hello.saySomething("Artificial Intelligence")
         );
     }
 }
