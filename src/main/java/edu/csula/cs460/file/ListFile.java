9cfeb7cd68877bb6e6c6c6d53928ebcdf2a359a2
Eric Liao <rcliao01@gmail.com>
2015-10-03 14:20:39 -0700

    implement solution to exercise

--- src/main/java/edu/csula/cs460/file/ListFile.java
+++ src/main/java/edu/csula/cs460/file/ListFile.java
@@ -1,21 +1,49 @@
 package edu.csula.cs460.file;
 
+import java.util.Arrays;
+import java.util.Scanner;
 import java.util.Map;
 import java.util.List;
-import java.util.ArrayList;
+import java.io.File;
+import java.io.IOException;
 
-import com.google.common.collect.Lists;
+import com.google.common.collect.ArrayListMultimap;
+import com.google.common.collect.ListMultimap;
+import com.google.common.collect.Multimaps;
 
 public class ListFile {
-    private Map<String, List<String>> adjancencyList;
+    // try to make *all* variables final as possible
+    private final Map<String, List<String>> adjancencyList;
 
     public ListFile(String filepath) {
-        // TODO: read file from filepath ('exercise-1/list-1.txt' for
-        // example) and parse line by line to fill out adjancencyList
+        // ListMultimap is a very useful class when it comes to Map of key
+        // to a Collection of values
+        ListMultimap<String, String> multimap = ArrayListMultimap.create();
+
+        // use class loader to read file from `resources` folder
+        ClassLoader classLoader = getClass().getClassLoader();
+        File file = new File(classLoader.getResource(filepath).getFile());
+
+        // try with resources (auto closable)
+        try (Scanner in = new Scanner(file)) {
+            while (in.hasNextLine()) {
+                // consider Java doesn't have destruct, we will need to
+                // create a temp variable to hodl both key and values
+                String[] parts = in.nextLine().split(":");
+                // stream api power with lambda expression
+                Arrays.stream(parts[1].split(" "))
+                    .forEach(value -> multimap.put(parts[0], value));
+            }
+        } catch (IOException e) {
+            // in case of error, always log error!
+            System.err.println("ListFile has trouble reading file");
+            e.printStackTrace();
+        }
+
+        adjancencyList = Multimaps.asMap(multimap);
     }
 
     public List<String> getList(String key) {
-        // TODO: get List of String for specific key
-        return Lists.newArrayList();
+        return adjancencyList.get(key);
     }
 }
--- src/main/java/edu/csula/cs460/file/MatrixFile.java
+++ src/main/java/edu/csula/cs460/file/MatrixFile.java
@@ -1,20 +1,49 @@
 package edu.csula.cs460.file;
 
+import com.google.common.collect.Lists;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.io.File;
+import java.io.IOException;
+
 public class MatrixFile {
-    private int[][] matrix;
+    private final int[][] matrix;
 
     public MatrixFile(String filepath) {
-        // TODO: read file from filepath ('exercise-1/2d-array-1.txt' for
-        // example) and parse line by line to fill out matrix
+        // since we don't know how many rows there is going to be, we will
+        // create a list to hold it instead
+        List<int[]> dynamicMatrix = Lists.newArrayList();
+
+        // use class loader to read file from `resources` folder
+        ClassLoader classLoader = getClass().getClassLoader();
+        File file = new File(classLoader.getResource(filepath).getFile());
+
+        try (Scanner in = new Scanner(file)) {
+            while (in.hasNextLine()) {
+                dynamicMatrix.add(
+                    Arrays.stream(in.nextLine().split(" "))
+                        .mapToInt(value -> Integer.parseInt(value))
+                        .toArray()
+                );
+            }
+        } catch (IOException e) {
+            // in case of error, always log error!
+            System.err.println("MatrixFile has trouble reading file");
+            e.printStackTrace();
+        }
+
+        matrix = dynamicMatrix.stream().toArray(int[][]::new);
     }
 
     public int getValue(int row, int col) {
-        // TODO: get value of a specific row and column (starting index is 0)
-        return 0;
+        return matrix[row][col];
     }
 
     public int getSum() {
-        // TODO: return the sum of all numbers in matrix
-        return 0;
+        return Arrays.stream(matrix)
+            .mapToInt(row -> Arrays.stream(row).sum())
+            .sum();
     }
 }
