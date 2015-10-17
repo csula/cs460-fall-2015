62833fd8e7c84c5256cc1783e4dc85de6f08044b
steveshim <steveyshim@gmail.com>
2015-10-17 12:13:51 -0700

    Test

--- src/main/java/edu/csula/cs460/graph/strategy/ObjectOriented.java
+++ src/main/java/edu/csula/cs460/graph/strategy/ObjectOriented.java
@@ -4,7 +4,9 @@ import edu.csula.cs460.graph.Edge;
 import edu.csula.cs460.graph.Node;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.List;
+import java.util.Scanner;
 
 public class ObjectOriented implements Representation {
     private List<Node> nodes;
@@ -12,6 +14,16 @@ public class ObjectOriented implements Representation {
 
     protected ObjectOriented(File file) {
         // TODO: parse file content and add it to nodes
+        try {
+            Scanner sc = new Scanner(file);
+            while (sc.hasNext()) {
+
+            }
+            sc.close();
+        } catch (IOException e) {
+            System.err.println("There is an error reading the file.");
+            e.printStackTrace();
+        }
     }
 
     @Override
