c73e3a771e6a4bf5a7a9d0402fba4469ab1d874d
Eric Liao <rcliao01@gmail.com>
2015-11-08 12:14:28 -0800

    update test case to reflect actual tests

--- src/test/java/edu/csula/cs460/graph/search/AStarTest.java
+++ src/test/java/edu/csula/cs460/graph/search/AStarTest.java
@@ -1,6 +1,9 @@
 package edu.csula.cs460.graph.search;
 
+import com.google.common.base.Stopwatch;
 import edu.csula.cs460.graph.Graph;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.io.File;
@@ -9,14 +12,38 @@ import static junit.framework.TestCase.assertEquals;
 
 public class AStarTest {
     // used for bonus
-    private Graph graph;
+    private Stopwatch timer;
+    private ClassLoader classLoader;
 
-    @Test(timeout=1000)
+    @Test
+    // a dummy test to warm up JVM machine about variables we use
+    public void warmup() {
+        File file1 = new File(classLoader.getResource("homework-3/grid-1.txt").getFile());
+        File file2 = new File(classLoader.getResource("homework-3/grid-2.txt").getFile());
+        File file3 = new File(classLoader.getResource("homework-3/grid-3.txt").getFile());
+        File file4 = new File(classLoader.getResource("homework-3/grid-4.txt").getFile());
+        File file5 = new File(classLoader.getResource("homework-3/grid-5.txt").getFile());
+
+        new AStar().searchFromGridFile(file1);
+        new AStar().searchFromGridFile(file2);
+        new AStar().searchFromGridFile(file3);
+        new AStar().searchFromGridFile(file4);
+        new AStar().searchFromGridFile(file5);
+    }
+
+    @Before
+    public void setUp() {
+        classLoader = getClass().getClassLoader();
+        timer = Stopwatch.createStarted();
+    }
+
+    @Test(timeout=5)
     public void testAStar1() {
-        ClassLoader classLoader = getClass().getClassLoader();
         File file = new File(classLoader.getResource("homework-3/grid-1.txt").getFile());
 
         String result = new AStar().searchFromGridFile(file);
+
+        System.out.println("A star 1 spends " + timer.stop());
 
         assertEquals(
             "Test grid 1 from key point to key point",
@@ -25,12 +52,13 @@ public class AStarTest {
         );
     }
 
-    @Test(timeout=1000)
+    @Test(timeout=5)
     public void testAStar2() {
-        ClassLoader classLoader = getClass().getClassLoader();
         File file = new File(classLoader.getResource("homework-3/grid-2.txt").getFile());
 
         String result = new AStar().searchFromGridFile(file);
+
+        System.out.println("A star 2 spends " + timer.stop());
 
         assertEquals(
             "Test grid 2 from key point to key point",
@@ -39,12 +67,13 @@ public class AStarTest {
         );
     }
 
-    @Test(timeout=1000)
+    @Test(timeout=5)
     public void testAStar3() {
-        ClassLoader classLoader = getClass().getClassLoader();
         File file = new File(classLoader.getResource("homework-3/grid-3.txt").getFile());
 
         String result = new AStar().searchFromGridFile(file);
+
+        System.out.println("A star 3 spends " + timer.stop());
 
         assertEquals(
             "Test grid 3 from key point to key point",
@@ -53,30 +82,33 @@ public class AStarTest {
         );
     }
 
-    @Test(timeout=1000)
+    @Test(timeout=30)
     public void testAStar4() {
-        ClassLoader classLoader = getClass().getClassLoader();
         File file = new File(classLoader.getResource("homework-3/grid-4.txt").getFile());
 
         String result = new AStar().searchFromGridFile(file);
 
+        System.out.println("A star 4 spends " + timer.stop());
+
         assertEquals(
             "Test grid 4 from key point to key point",
-            "SSSSEESESSWWWSSSSEESESS",
+            "ESSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSESSSSSSSSSSSSSSSSSSSSSS",
             result
         );
+
     }
 
-    @Test(timeout=1000)
+    @Test(timeout=5)
     public void testAStar5() {
-        ClassLoader classLoader = getClass().getClassLoader();
         File file = new File(classLoader.getResource("homework-3/grid-5.txt").getFile());
 
         String result = new AStar().searchFromGridFile(file);
 
+        System.out.println("A star 5 spends " + timer.stop());
+
         assertEquals(
             "Test grid 5 from key point to key point",
-            "SSSSEESESSWWWSSSSEESESSSWWWSSESEES",
+            "SSSSEESESSWSWWSSSSEEESSSWWWSSSEESE",
             result
         );
     }
